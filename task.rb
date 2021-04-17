2aa0f1d97c3d4d012a8063dfcf8e6cdc22966aa6
大津　秀介 <otsu0412@yahoo.co.jp>
2021-04-18 06:50:51 +0900

    q4~q6の解答

--- task.rb
+++ task.rb
@@ -29,6 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+ sports.compact!
 
   # 以下は変更しないで下さい
   p sports
@@ -39,15 +40,17 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts array1.empty?
+  puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
-end
+    numbers2 = numbers1.map{|numbers1|numbers1*10}
+    puts numbers2
+end  
 
 def q7
   array = ["1", "2", "3", "4", "5"]
