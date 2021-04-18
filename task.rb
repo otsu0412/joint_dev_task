33316302c5ba52a07b94a45ad69f733ed671bbd7
大津　秀介 <otsu0412@yahoo.co.jp>
2021-04-19 05:54:10 +0900

    Q7~Q10の解答

--- task.rb
+++ task.rb
@@ -56,6 +56,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array = array.map(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -65,7 +66,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,14 +77,22 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name,  i| 
+   puts  "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do | food |
+    if food.include?("うに")
+      p  "好物です"
+    else
+      p "まあまあ好きです"
+    end
+  end
 end
 
 def q11
