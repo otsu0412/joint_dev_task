059de3758837a9e620c4dae10a4192191d4f4066
大津　秀介 <otsu0412@yahoo.co.jp>
2021-04-21 20:04:50 +0900

    Q15~Q16の解答

--- task.rb
+++ task.rb
@@ -134,7 +134,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.key?(:age) ? 'OK' : 'NG'
+  puts data2.key?(:age) ? 'OK' : 'NG'
 end
 
 def q16
@@ -146,7 +147,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+ users.each  do |user|
+   puts  "私の#{user[:name]} です。 年齢は#{user[:age]}です。"
+  end
 end
 
 class UserQ17
